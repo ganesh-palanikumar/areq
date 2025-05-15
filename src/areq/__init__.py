83407bc3e12ee482551070be3bf9118a99f09d39
Ganesh Palanikumar <ganeshpkumar93+github@gmail.com>
2025-05-15 01:29:45 -0700

    Update README

--- src/areq/__init__.py
+++ src/areq/__init__.py
@@ -1,3 +1,4 @@
 from .api import get, options, head, post, put, patch, delete
+from .models import AreqResponse
 
-__all__ = ['get', 'options', 'head', 'post', 'put', 'patch', 'delete']
+__all__ = ["get", "options", "head", "post", "put", "patch", "delete", "AreqResponse"]
